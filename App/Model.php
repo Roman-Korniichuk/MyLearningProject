806038d7c1278aa8e3892733bb8888619ed39bf7
Roman Korniichuk <jerrat@bigmir.net>
2017-02-23 15:24:53 +0200

    Added method 'findById()' to Model class

--- App/Model.php
+++ App/Model.php
@@ -8,9 +8,24 @@ namespace App;
 abstract class Model 
 {
     const TABLE = '';
+    
     public static function getAll()
     {
         $db = new DB;
         return $db->query('SELECT * FROM ' . static::TABLE, static::class);
+    }
+    public static function findById($id)
+    {
+        $db = new DB;
+        $data = $db->query(
+                'SELECT * FROM ' . static::TABLE . ' WHERE id=:id',
+                static::class,
+                [':id' => $id]
+                );
+        if (count($data) > 0) {
+            return $data;
+        } else {
+            return false;
+        }
     }
 }
