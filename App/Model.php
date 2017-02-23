6136a99595320a297562138c471473798162fd4c
Roman Korniichuk <jerrat@bigmir.net>
2017-02-24 00:25:31 +0200

    Realized CRUD methods in Model abstract class

--- App/Model.php
+++ App/Model.php
@@ -5,7 +5,9 @@ namespace App;
 abstract class Model 
 {
     const TABLE = '';
-    
+    public $id;
+
+
     public static function getAll()
     {
         $db = DB::instance();
@@ -23,6 +25,74 @@ abstract class Model
             return $data[0];
         } else {
             return false;
+        }
+    }
+    public function isNew()
+    {
+        return empty($this->id);
+    }
+    public function insert()
+    {
+        if (!$this->isNew()) {
+            return;
+        } else {
+            $cols = [];
+            $vals = [];
+            foreach ($this as $key=>$val) {
+                if ('id' == $key) {
+                    continue;
+                } else {
+                    $cols[] = $key;
+                    $vals[':'.$key] = $val;
+                }
+            }
+            $sql = 'INSERT INTO ' . static::TABLE . '(' . implode(',', $cols) . ')' .
+                    'VALUES (' . implode(',', array_keys($vals)) . ')';
+            $db = DB::instance();
+            $db->execute($sql, $vals);
+        }
+    }
+    public function update($id)
+    {
+        $object = static::findById($id);
+        if (!$object) {
+            return false;
+        } else {
+            $cols = [];
+            $vals = [];
+            foreach ($this as $key=>$val) {
+                if (('id' == $key)||(null == $val)) {
+                    continue;
+                } else {
+                    $cols[] = $key . '=:' . $key;
+                    $vals[':'.$key] = $val;
+                }
+            }
+            $sql = 'UPDATE ' . static::TABLE . ' SET ' . implode(',',$cols) .
+                        ' WHERE id=:id';
+            $vals[':id'] = $id;
+            $db = DB::instance();
+            $db->execute($sql, $vals);
+        }
+    }
+    public function save()
+    {
+       if ($this->isNew()) {
+           return $this->insert();
+       } else {
+           return $this->update($this->id);
+       }
+    }
+    public function delete()
+    {
+        // DELETE FROM articles WHERE id=42
+        if ($this->isNew()) {
+            return false;
+        } else {
+            $sql = 'DELETE FROM ' . static::TABLE . ' WHERE id=:id';
+            $data = [':id'=> $this->id];
+            $db = DB::instance();
+            $db->execute($sql, $data);
         }
     }
 }
