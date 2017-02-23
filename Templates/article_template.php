20571feb78284bfd4ff5f7ec3d53f52c6ab7de1c
Roman Korniichuk <jerrat@bigmir.net>
2017-02-23 15:51:34 +0200

    Added templates: main page (index) and article page

---
+++ Templates/article_template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $article->getHeader(); ?></title>
+    </head>
+    <body>
+        <h1><?php echo $article->getHeader(); ?></h1><br>
+        <div>
+            <?php echo $article->getText(); ?> <br>
+            <span class="author">Author: <?php echo $article->getAuthor(); ?></span>
+        </div>
+        <a href="../index.php">Back</a>
+    </body>
+</html>
---
+++ Templates/index_template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            foreach ($news as $article) {
+        ?>
+        <div class="article">
+            <h2><?php echo $article->getHeader(); ?></h2>
+            <div class="desc">
+                <?php echo $article->getDescription(); ?> <br>
+                <a href="../article.php?id=<?php echo $article->getId(); ?>">Read more</a> <br> <hr>
+            </div>
+        </div>
+            <?php } ?>
+    </body>
+</html>
