3e809e602dc6eda90966e46712c7e82aa939e160
Roman Korniichuk <jerrat@bigmir.net>
2017-02-23 15:52:58 +0200

    Modify index.php : connection with template

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
 include __DIR__ . '/autoload.php';
+
+$page = new \App\View();
+$articles = App\Models\Article::getAll();
+$page->assign('news', $articles);
+$page->display(__DIR__ . '/Templates/index_template.php');
