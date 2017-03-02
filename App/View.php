8fd9ba50e09327e9290032b9413498e1e115c087
Roman Korniichuk <jerrat@bigmir.net>
2017-03-02 15:26:58 +0200

    Modified View class: add render() method and update display() method

--- App/View.php
+++ App/View.php
@@ -3,17 +3,33 @@
 namespace App;
 
 class View 
+    implements \Countable
 {
     use MagicSet;
     protected $data;
     
     public function display($template) 
     {
+        echo $this->render($template);
+    }
+    
+    public function render($template)
+    {
+        ob_start();
         if (!empty($this->data)) {    
             foreach ($this->data as $name=>$value) {
                $$name = $value;
             }
         }
         include $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+
+    public function count() 
+    {
+        return count($this->data);
     }
 }
