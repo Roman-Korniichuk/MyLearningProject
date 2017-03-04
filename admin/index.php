2d6f09ccf4178b51bc9fabd9cf39f11bf90eb52d
Roman Korniichuk <jerrat@bigmir.net>
2017-03-04 13:56:34 +0200

    Update admin panel files

--- admin/index.php
+++ admin/index.php
@@ -2,11 +2,15 @@
 
 include __DIR__ . '/../autoload.php';
 
-$articles = App\Models\Article::getAll();
+/*$articles = App\Models\Article::getAll();
 
 $page = new \App\View();
 //$page->assign('articles', $articles);
 $page->articles = $articles;
 $page->display(__DIR__ . '/../Templates/admin_template.php');
 //echo 'Here will be admin-panel, protected by .htaccess';
+ */
 
+$act = $_GET['act'] ?: 'Index';
+$action = new App\Controllers\Admin;
+$action->action($act);
--- admin/update.php
+++ admin/update.php
@@ -1,28 +1,19 @@
 <?php
 include __DIR__ . '/../autoload.php';
 
-$page = new \App\View();
-
-if (isset($_GET['id'])) {
-    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
-    $article = App\Models\Article::findById($id);
-    
-} else {
-    $article = new App\Models\Article();
-}
-
-if ((isset($_POST['author']))&&(isset($_POST['header_article']))&&(isset($_POST['text']))) {
-    $author = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_STRING);
+if ((isset($_POST['author_id']))&&(isset($_POST['header_article']))&&(isset($_POST['text']))) {
+    if (isset($_POST['id'])) {
+        $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+        $article = \App\Models\Article::findById($id);
+    } else {
+        $article = new \App\Models\Article();
+    }
+    $author = filter_input(INPUT_POST, 'author_id', FILTER_SANITIZE_STRING);
     $header = filter_input(INPUT_POST, 'header_article', FILTER_SANITIZE_STRING);
     $text = filter_input(INPUT_POST, 'text', FILTER_SANITIZE_STRING);
-    $article->author = $author;
+    $article->author_id = $author;
     $article->header = $header;
     $article->text = $text;
     $article->save();
     header('location:index.php');
 }
-
-//$page->assign('article', $article);
-$page->article = $article;
-$page->display(__DIR__ . '/../Templates/admin_update.php');
-
