16dd962f656029012d289a7298d6ba4f3f9d02f9
Roman Korniichuk <jerrat@bigmir.net>
2017-03-07 21:25:44 +0200

    Deleted old commented code. No changes in working code

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 
 include __DIR__ . '/autoload.php';
 
-//$page = new \App\View();
-//$news = new \App\Models\News;
-//$page->news = $news->getLastNews();
-//$page->assign('news', $articles);
-//$page->display(__DIR__ . '/Templates/index_template.php');
 $act = $_GET['act'] ?: 'Index';
 try {
 $action = new App\Controllers\News;
