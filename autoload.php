b1e08ef8b6686457d29d5e620634e80d65d0e1b6
Roman Korniichuk <jerrat@bigmir.net>
2017-03-07 21:24:40 +0200

    Fix autoload: spl_autoload_register + composer autoload

--- autoload.php
+++ autoload.php
@@ -1,8 +1,7 @@
 <?php
 
-function __autoload($class)
-{
-    require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-}
+spl_autoload_register(function ($class) {
+    include __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+});
 
 include __DIR__ . '/vendor/autoload.php';
